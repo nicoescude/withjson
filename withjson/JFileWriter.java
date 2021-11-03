b4142bd785702045e8f26fa8f30ccdcd9b1bdf61
nicoescude <92390695+nicoescude@users.noreply.github.com>
2021-11-03 01:04:52 -0300

    Modularization of JFileWriter and JFileReader

---
+++ withjson/JFileWriter.java
@@ -0,0 +1,38 @@
+package com.nescude.withjson;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONWriter;
+
+public class JFileWriter {
+
+    public static void writeJSON(JSONObject object, String filename){
+        String s = object.toString();
+        writeDefault(s, filename);
+    }
+
+    public static void writeJSONArray(JSONArray array, String filename){
+        String s = array.toString();
+        writeDefault(s, filename);
+    }
+
+    private static void writeDefault(String s, String filename){
+        try {
+            File f = new File(filename);
+            FileWriter fw = new FileWriter(f);
+
+            fw.write(s);
+            fw.flush();
+            fw.close();
+
+            System.out.println("Archivo json escrito con exito!");
+        } catch (IOException e) {
+            System.out.println("Error de E/S: "+e.getMessage());
+        }
+    }
+}
